65d7d07e9d863425a5abafa4708105142969793b
Jyrki Puttonen <jyrki.puttonen@sysart.fi>
2016-01-21 12:36:11 +0200

    Add logo and corner image
    Had to change body position as otherwise the text would have
    dropped far too low.

--- Dockerfile
+++ Dockerfile
@@ -2,3 +2,4 @@ FROM node:0.10
 RUN git clone https://github.com/hakimel/reveal.js.git
 RUN cd reveal.js && npm install
 RUN npm install -g grunt-cli
+ADD imgs /reveal.js/imgs
--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 set -o errexit
 set -o nounset
 
-docker run -ti -v $(pwd)/index.html:/reveal.js/index.html -v $(pwd)/sysart.css:/reveal.js/css/theme/sysart.css -p 8000:8000 sysart-node /bin/bash
+docker run -ti -v $(pwd)/index.html:/reveal.js/index.html -v $(pwd)/sysart.css:/reveal.js/sysart.css -v $(pwd)/imgs/:/reveal.js/imgs/ -p 8000:8000 sysart-node
