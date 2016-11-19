592f59f643647a457b3c91558e4a2dd0fc8e30b5
Jyrki Puttonen <jyrkiput@gmail.com>
2016-11-19 12:59:53 +0200

    Use new theme
    Changing jobs, changing themes

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ RUN git clone https://github.com/hakimel/reveal.js.git
 RUN cd reveal.js && npm install
 RUN npm install -g grunt-cli
 ADD theme_images /reveal.js/theme_images
-ADD sysart.css /reveal.js/sysart.css
+ADD symbio.css /reveal.js/css/theme/symbio.css
 WORKDIR /reveal.js/
 ENTRYPOINT ["grunt", "serve"]
 EXPOSE 8000
--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 set -o errexit
 set -o nounset
 
-docker run --rm -ti -v $(pwd)/index.html:/reveal.js/index.html -v $(pwd)/sysart.css:/reveal.js/sysart.css -v $(pwd)/theme_images/:/reveal.js/theme_images/ -P sysart-node
+docker run --rm -ti -v $(pwd)/index.html:/reveal.js/index.html -v $(pwd)/css/theme/symbio.css:/reveal.js/css/theme/symbio.css -v $(pwd)/theme_images/:/reveal.js/theme_images/ -P sysart-node
